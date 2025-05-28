80ea89a5adfb6f2754eadc1c897c82538f673eb4
Tiago <tiagoportilho52@gmail.com>
2025-05-28 20:31:46 -0300

    Initial construct

---
+++ src/test/java/com/tiago/crud/CrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tiago.crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
